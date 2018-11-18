ea8e107b7ed19c277ab0743b813eabb7de96f52c
Angela Dong <angela.dong@berkeley.edu>
2018-11-18 03:52:41 -0800

    routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import LandingApp from './apps/landingApp';
+import ProjectsApp from './apps/projectsApp';
+import AboutApp from './apps/aboutApp';
+
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Navbar className='short' inverse collapseOnSelect fixedTop>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <Link to='/home'>
+                  <img src={logo} className='logo' alt='logo'/>
+                  <h3 className='title'>Design Department</h3>
+                </Link>
+              </Navbar.Brand>
+            <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <LinkContainer to='/home'>
+                  <NavItem className='page' eventKey={1}>Home</NavItem>
+                </LinkContainer>
+                <LinkContainer to='/projects'>
+                  <NavDropdown className='page' inverse eventKey={2} title="Projects" id="basic-nav-dropdown">
+                    <LinkContainer to='/projects/edu'><MenuItem eventKey={2.1}>Design x Education</MenuItem></LinkContainer>
+                    <LinkContainer to='/projects/uiux'><MenuItem eventKey={2.2}>UI/UX Research</MenuItem></LinkContainer>
+                    <LinkContainer to='/projects/misc'><MenuItem eventKey={2.3}>Miscellaneous</MenuItem></LinkContainer>
+                  </NavDropdown>
+                </LinkContainer>
+                <LinkContainer to='/about'>
+                  <NavItem className='page' eventKey={3}>About</NavItem>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">
+                {/*Medium*/}
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                {/*Insta or whatever*/}
+              </NavItem>
+            </Nav>
+          </Navbar>
+          <Route exact path='/' component={LandingApp} />
+          <Route path='/home' component={LandingApp} />
+          <Route path='/projects' component={ProjectsApp} />
+          <Route path='/projects/edu' component={ProjectsApp} />
+          <Route path='/projects/uiux' component={ProjectsApp} />
+          <Route path='/projects/misc' component={ProjectsApp} />
+          <Route path='/about' component={AboutApp} />
+        </div>
+      </Router>
     );
   }
 }
-
-export default App;
